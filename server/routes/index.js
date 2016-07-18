ce4a2a6b526233d0314000b8861a036c6ac1f079
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-17 23:48:21 -0700

    Add /db route to view all users in db, for deployment testing purposes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,16 @@ var express = require('express');
 
 var router = express.Router();
 
+var db = require('../db');
+
 // USER-FACING ROUTES ======================================
 
 // catch-all route for client-side routing
-router.get('*', function(req, res) {
-  res.sendFile('/index.html');
+router.get('/db', function(req, res) {
+  console.log('inside db route');
+  db.User.findAll().then(function(users) {
+    res.json(users);
+  });
 });
 
 module.exports = router;
