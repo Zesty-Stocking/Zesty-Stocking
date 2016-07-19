40d9b10d78806053076982af2ccc1f7b99f97945
Jason Davenport <jason.arkens17@gmail.com>
2016-07-19 12:58:08 -0700

    made modifications to original api routes for users

--- server/routes/api.js
+++ server/routes/api.js
@@ -16,26 +16,61 @@ var router = express.Router();
 router.route('/users')
 
   .post(function(req, res) {
-    var user = new User();
-
     user.username = req.body.username;
     user.name  = req.body.name;
     user.location = req.body.loction;
     user.avatarUrl = req.body.avatarUrl;
 
+    user.username.find(function(err, found) {
+      if(err)
+        res.send(err);
+      if(found)
+        res.send('User already exsists!');
+    })
+
+    var user = new User();
     user.save(function(err) {
       if(err)
         res.send(err);
       //What should we send back upon creation?
-      res.json({message: 'User created!'})
+      res.json(user); 
     });
 
     .get(function(req, res) {
       User.findAll().then(function(users) {
-        res.json(users); 
+        res.json(users);
+      });
+    });
+
+  });
+
+  router.route('/users/:user_id')
+    .get(function(req, res) {
+      User.findById(req.params.user_id, function(err, user) {
+        if(err)
+          res.send(err);
+        res.json(user);
       })
-    })
+
+      .put(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+          if(err)
+            res.send(err);
+            //What would we want to be letting the user change here?
+          user.something = req.body.something;
+
+          user.save(function(err) {
+            if(err)
+              res.send(err);
+
+            res.json({ message: 'User updated!' })
+          })
+        })
+      })
+    });
+
+router.route('/messages')
+
+  .post(function(req, res) {
 
   })
-
-module.exports = router;
