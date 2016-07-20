fd442df4df276deb46c81a799ad19e4124270b85
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-07-20 15:29:25 -0700

    Align Posts messages to top

--- client/zestyreactnative/src/components/timeline/posts.js
+++ client/zestyreactnative/src/components/timeline/posts.js
@@ -57,8 +57,6 @@ class Posts extends Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: 'white'
   }
 });
