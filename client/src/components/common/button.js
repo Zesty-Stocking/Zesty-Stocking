bca14596eaa78e67cc883647c7310c5e91d0d853
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-07-19 21:21:54 -0700

    Refactor Button component
    - change all tabs to spaces
      - renders easier, in my opinion
    - convert the Button component from a class to stateless functional
      - for now, there is no state in here. So, no need for the extra
        complications that come with class components.

--- client/src/components/common/button.js
+++ client/src/components/common/button.js
@@ -1,41 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import {
-	View,
-	Text,
-	StyleSheet,
-	TouchableHighlight
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight
 } from 'react-native';
 
-class Button extends Component {
-	render() {
-		return (
-			<TouchableHighlight 
-			style={styles.button}
-			underlayColor={'gray'}
-			onPress={this.props.onPress}
-			>
-				<Text style={styles.buttonText}>{this.props.text}</Text>
-			</TouchableHighlight>
-		);
-	}
+var Button = ({ buttonText, onPress }) => {
+  render() {
+    return (
+      <TouchableHighlight
+        style={ styles.button }
+        underlayColor={ 'gray' }
+        onPress={ onPress }
+      >
+        <Text style={ styles.buttonText }>{ text }</Text>
+      </TouchableHighlight>
+    );
+  }
 };
 
 var styles = StyleSheet.create({
-	button: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderWidth: 1,
-		borderRadius: 5,
-		padding: 5,
-		borderColor: 'black',
-		marginTop: 10
-	},
-	buttonText: {
-		flex: 1,
-		alignSelf: 'center',
-		fontSize: 20
-	}
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    borderColor: 'black',
+    marginTop: 10
+  },
+  buttonText: {
+    flex: 1,
+    alignSelf: 'center',
+    fontSize: 20
+  }
 });
 
 export default Button;
