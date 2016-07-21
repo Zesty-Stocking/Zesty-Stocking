6c86fc752a414ec9349895f16a68255895bde34d
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-07-20 18:21:03 -0700

    Get posts from DB to show up in timeline

--- client/zestyreactnative/src/components/timeline/posts.js
+++ client/zestyreactnative/src/components/timeline/posts.js
@@ -6,30 +6,70 @@ import {
 } from 'react-native';
 import Message from '../message/view';
 import { border } from '../../../helpers/scaffolding';
+// import { getMessages } from '../../../helpers/api';
+
+var ipAddress = '10.6.24.5';
+const baseUrl = `http://${ipAddress}:4568/api`;
 
 var dummyMessages = [
   { user: 'Bronson', text: 'Nom nom nom', likes: -1 },
   { user: 'Fifo', text: 'First in, first out. I mean: Woof!', likes: -1 }
 ];
 
+//var testFetch = (url) => {
+//  return fetch(url, {
+//    method: 'GET',
+//    headers: {
+//      'Accept': 'application/json',
+//      'Content-Type': 'application/json',
+//    },
+//    body: null
+//  })
+//};
+
+var getMessages = () => {
+  var url = `${baseUrl}/messages`;
+
+  return fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      return json;
+    })
+    .catch(function(err) {
+      return err;
+    })
+};
+
 class Posts extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      messages: dummyMessages
+      messages: dummyMessages,
+      errorMessage: ''
     };
 
     this.listOfMessages = this.listOfMessages.bind(this);
   }
 
+  componentWillMount() {
+    getMessages()
+      .then((json) => {
+        this.setState({ messages: json });
+      })
+      .catch((err) => {
+        this.setState({ errorMessage: err });
+      });
+  }
+
   listOfMessages() {
     messages = this.state.messages || [];
 
-    var renderedMessage = ({ user, text, likes }, index) => {
+    var renderedMessage = ({ text, likes }, index) => {
       return (
         <Message
-          user={ user }
           text={ text }
           likes={ likes }
           key={ index }
