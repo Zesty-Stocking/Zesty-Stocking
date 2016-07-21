b1184edd517e8197ffbe88b1df112c0372883c7c
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-07-20 18:29:10 -0700

    Refactor API helper & its use in Posts component

--- client/zestyreactnative/helpers/api.js
+++ client/zestyreactnative/helpers/api.js
@@ -1,27 +1,11 @@
-const baseUrl = 'http://localhost:4568/api';
-
-var fetchJSON = (url) => {
-  return fetch(url)
-    .then(function(response){
-      return response.json();
-    })
-    .catch(function(err) {
-      return err;
-    });
-};
+var ipAddress = '10.6.24.5';
+const baseUrl = `http://${ipAddress}:4568/api`;
 
 module.exports.getMessages = () => {
   var url = `${baseUrl}/messages`;
 
   return fetch(url)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(json) {
-      return json;
-    })
-    .catch(function(err) {
-      console.log(err);
-      return err;
-    })
+    .then((response) => response.json() )
+    .then((json) => json )
+    .catch((err) =>  err );
 };
--- client/zestyreactnative/src/components/timeline/posts.js
+++ client/zestyreactnative/src/components/timeline/posts.js
@@ -6,41 +6,12 @@ import {
 } from 'react-native';
 import Message from '../message/view';
 import { border } from '../../../helpers/scaffolding';
-// import { getMessages } from '../../../helpers/api';
-
-var ipAddress = '10.6.24.5';
-const baseUrl = `http://${ipAddress}:4568/api`;
+import { getMessages } from '../../../helpers/api';
 
 var dummyMessages = [
   { user: 'Bronson', text: 'Nom nom nom', likes: -1 },
   { user: 'Fifo', text: 'First in, first out. I mean: Woof!', likes: -1 }
 ];
-
-//var testFetch = (url) => {
-//  return fetch(url, {
-//    method: 'GET',
-//    headers: {
-//      'Accept': 'application/json',
-//      'Content-Type': 'application/json',
-//    },
-//    body: null
-//  })
-//};
-
-var getMessages = () => {
-  var url = `${baseUrl}/messages`;
-
-  return fetch(url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(json) {
-      return json;
-    })
-    .catch(function(err) {
-      return err;
-    })
-};
 
 class Posts extends Component {
   constructor(props) {
@@ -56,12 +27,8 @@ class Posts extends Component {
 
   componentWillMount() {
     getMessages()
-      .then((json) => {
-        this.setState({ messages: json });
-      })
-      .catch((err) => {
-        this.setState({ errorMessage: err });
-      });
+      .then((json) => this.setState({ messages: json }) )
+      .catch((err) => this.setState({ errorMessage: err }) )
   }
 
   listOfMessages() {
