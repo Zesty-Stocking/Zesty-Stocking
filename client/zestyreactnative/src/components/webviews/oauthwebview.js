fdf29f478332525afb78f54d949300e41059d0bc
Pranay M <pranay@utexas.edu>
2016-07-21 11:31:53 -0700

    check for accesstoken url, store token and redirect to posts view

--- client/zestyreactnative/src/components/webviews/oauthwebview.js
+++ client/zestyreactnative/src/components/webviews/oauthwebview.js
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 import {
 	WebView,
 	StyleSheet,
-	View
+	View,
+	AsyncStorage
 } from 'react-native';
 import NavigationBar from 'react-native-navbar';
+
+const TOKEN_URL = 'http://localhost:4568/account?'
 
 class OAuthWebView extends Component {
 	render() {
@@ -35,8 +38,26 @@ class OAuthWebView extends Component {
 		//This is where we check whether the url is our payload url
 	 	// if so, we exit webview and log in the user
 	 	console.log('loading new page');
-		console.log(navState);
-
+		console.log('navState object:', navState);
+		//check URL and only do it when navstate.loading is false, meaning page is done loading
+		if (navState.url.startsWith(TOKEN_URL) && navState.loading === false) {
+			//grab access token
+			var tokenIndex = navState.url.indexOf('accessToken=') + 12;
+			var accessToken = navState.url.substring(tokenIndex, navState.url.length);
+			//store accessToken in asyncStorage
+			AsyncStorage.setItem('accessToken', accessToken)
+				.then(() => {
+					console.log('set access token in async storage:', accessToken);
+					//exit webview here -
+					//we need to leave the context of authentication views (see udemy course, lecture 69)
+					//reset route stack and push posts route on navigator
+					this.props.navigator.immediatelyResetRouteStack([{ name: 'posts' }]);
+					//If you need to get access token again after this, it should be stored in asyncStorage
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		}
 	}
 	onBackPress() {
 		// alert('go back!');
