aecc5e1c2af0111c96e7cb14c03e39bcbf9ccdb3
Rafael Espinoza <nerddiffer@gmail.com>
2016-07-23 19:01:42 -0700

    Comment out console logs from OAuthWebView

--- client/zestyreactnative/src/components/webviews/oauthwebview.js
+++ client/zestyreactnative/src/components/webviews/oauthwebview.js
@@ -38,8 +38,8 @@ class OAuthWebView extends Component {
   onNavigationStateChange(navState) {
     //This is where we check whether the url is our payload url
     // if so, we exit webview and log in the user
-    console.log('loading new page');
-    console.log('navState object:', navState);
+    // console.log('loading new page');
+    // console.log('navState object:', navState);
     //check URL and only do it when navstate.loading is false, meaning page is done loading
     if (navState.url.startsWith(TOKEN_URL) && navState.loading === false) {
       //grab access token
@@ -48,7 +48,7 @@ class OAuthWebView extends Component {
       //store accessToken in asyncStorage
       AsyncStorage.setItem('accessToken', accessToken)
         .then(() => {
-          console.log('set access token in async storage:', accessToken);
+          // console.log('set access token in async storage:', accessToken);
           //exit webview here -
           //we need to leave the context of authentication views (see udemy course, lecture 69)
           //reset route stack and push posts route on navigator
