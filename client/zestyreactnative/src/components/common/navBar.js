fdecfb788f30c247d1c5d9e6a480a02628c780c9
Rafael Espinoza <nerddiffer@gmail.com>
2016-07-23 18:27:24 -0700

    Add some default values for common NavBar component

--- client/zestyreactnative/src/components/common/navBar.js
+++ client/zestyreactnative/src/components/common/navBar.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import NavigationBar from 'react-native-navbar';
 
-var NavBar = ({ titleConfig, leftButtonConfig }) => {
+const defaults ={
+  tintColor: '#333'
+};
+
+const defaultTitle = {
+  title: 'hashItOut',
+  tintColor: defaults.tintColor
+};
+
+const defaultLeftButton = {
+  title: '<=',
+  tintColor: defaults.tintColor
+};
+
+const defaultRightButton = {
+  title: '=>',
+  tintColor: defaults.tintColor
+};
+
+var NavBar = (props) => {
+  var titleConfig = props.title || defaultTitle;
+  var leftButtonConfig = props.leftButton || defaultLeftButton;
+  var rightButtonConfig = props.rightButton || defaultRightButton;
+
   return (
     <NavigationBar
       title={ titleConfig }
-      leftButton={ leftButtonConfig } />
+      leftButton={ leftButtonConfig }
+      rightButton={ rightButtonConfig }
+    />
   );
 };
 
