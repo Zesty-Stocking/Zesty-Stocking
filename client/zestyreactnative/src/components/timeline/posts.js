9f5f785b19d8f2c93c799d46fbc0a9e7d7bc8b2c
Rafael Espinoza <nerddiffer@gmail.com>
2016-07-24 21:19:28 -0700

    Implement a logoutUser function for Posts component

--- client/zestyreactnative/src/components/timeline/posts.js
+++ client/zestyreactnative/src/components/timeline/posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  AsyncStorage,
   StyleSheet,
   Text,
   View
@@ -7,17 +8,32 @@ import {
 import MessageList from '../message/messageList';
 import NavBar from '../common/navBar';
 import Button from '../common/button';
-import { getMessages } from '../../helpers/api';
+import { getMessages, getLogout } from '../../helpers/api';
+
+var logoutUser = (navigator) => {
+  getLogout()
+    .then(function() {
+      AsyncStorage.removeItem('accessToken');
+
+      var routeStack = [
+        { name: 'signin' }
+      ];
+
+      navigator.immediatelyResetRouteStack(routeStack);
+    });
+};
 
 class Posts extends Component {
   constructor(props) {
     super(props);
-    this.onPressCompose = this.onPressCompose.bind(this);
-    this.updateMessages = this.updateMessages.bind(this);
+
     this.state = {
       data: [],
       error: ''
     }
+
+    this.onPressCompose = this.onPressCompose.bind(this);
+    this.updateMessages = this.updateMessages.bind(this);
   }
 
   componentWillMount() {
@@ -41,7 +57,7 @@ class Posts extends Component {
     var leftButton = {
       title: 'Sign Out',
       tintColor: '#333',
-      handler: () => console.log('Tried to sign out')
+      handler: () => logoutUser(this.props.navigator)
     };
 
     return (
